f3948bc7550e7b8832585fe46109ec7776f47cff
Ron Hupa <mail@ronaldhupa.com>
2022-11-13 22:13:02 +0800

    Initial Commit

---
+++ RamdomPhoto/RamdomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamdomPhotoApp.swift
+//  RamdomPhoto
+//
+//  Created by Ronald Jay Hupa on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RamdomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
